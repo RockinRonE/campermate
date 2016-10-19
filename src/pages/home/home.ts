a27a13530d141d6bb65fabc5b74d90da7fb42f7e
Ronny Ewanek <ronny.ewanek@gmail.com>
2016-10-19 08:09:10 -0500

    setup module and home

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
+import { LocationPage } from '../location-page/location-page';
+import { MyDetailsPage } from '../my-details-page/my-details-page';
+import { CampDetailsPage } from '../camp-details-page/camp-details-page';
 
-import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
+  selector: 'home-page',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  tab1Root: any = LocationPage;
+  tab2Root: any = MyDetailsPage;
+  tab3Root: any = CampDetailsPage; 
+
+  constructor() {
     
   }
 
